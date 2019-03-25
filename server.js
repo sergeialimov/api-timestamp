9698f14e25cb51ebe734cdb453fc0ab6cad7d235
Sergei Alimov <8912652@gmail.com>
2019-03-25 19:34:48 +0300

    fix formatting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,12 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  res.json({ greeting: `hello ==== timestamp ${date_string}` });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
